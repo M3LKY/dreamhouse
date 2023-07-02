210d7f49c4c73f561b5d5d5beb0d2e4291c88eb1
M3LKY <melquisedecgonzalez74@gmail.com>
2023-07-02 03:56:53 -0400

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Videobg from './assets/img/ho.mp4'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Videobg from './assets/img/ho.mp4';
 
-import Home from './pages/Home'
-import PropertyDetails from './pages/PropertyDetails'
+import Home from './pages/Home';
+import PropertyDetails from './pages/PropertyDetails';
 
-function App() {
+import './App.css';
+
+const App = () => {
   return (
     <div className="app-container">
       <video className='video-bg' src={Videobg} autoPlay muted loop />
@@ -20,7 +22,7 @@ function App() {
         <Footer />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom'
-import HouseContextProvider from './components/HouseContext.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import HouseContextProvider from './components/HouseContext.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <HouseContextProvider>
     <Router>
@@ -12,4 +13,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </React.StrictMode>
     </Router>
   </HouseContextProvider>
-)
+);
